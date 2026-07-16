62aa3089d7d835b2466eb29a3a6a4c06301e9233
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-16 15:41:36 +0200

    Component: Footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white py-4">
+      <div className="container">
+        <div className="text-center">
+          <h5>FitLife</h5>
+          <p>Live healthier. Train smarter.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
